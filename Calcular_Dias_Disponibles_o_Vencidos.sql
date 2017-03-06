def00cd727d880198da665fc9df4c82da8613118
eisnerh <eisner.lopez@gmail.com>
2017-03-06 10:35:44 -0600

    Nueva Consulta
    Calcula los días Vencidos

---
+++ Calcular_Dias_Disponibles_o_Vencidos.sql
@@ -0,0 +1,21 @@
+Select 
+	`cabina`.`cabina_id`, 
+    `cabina`.`descripcion_cabina`, 
+    `cabina`.`estado_cabina`, 
+    `factura_cabina`.`fecha`, 
+    `factura_cabina`.`cant_dia`, 
+    date(@fecha := ((fecha) + (cant_dia))),
+    if (date(@fecha := ((fecha) + (cant_dia)))<now(),
+		DATEDIFF(date(@fecha := ((fecha) + (cant_dia))),now()),
+        -1*(DATEDIFF(date(@fecha := ((fecha) + (cant_dia))),now()))) as cantidad
+    from 
+    `pct3`.`factura_cabina` AS `factura_cabina`, 
+    `pct3`.`cabina` AS `cabina` 
+    WHERE `factura_cabina`.`cabina_cabina_id` = `cabina`.`cabina_id` 
+    and 
+    if (date(@fecha := ((fecha) + (cant_dia)))<now(),
+		-1*DATEDIFF(date(@fecha := ((fecha) + (cant_dia))),now()),
+        DATEDIFF(date(@fecha := ((fecha) + (cant_dia))),now())) > 0
+        and
+        `cabina`.`estado_cabina` = 'Ocupado'
+        ;
